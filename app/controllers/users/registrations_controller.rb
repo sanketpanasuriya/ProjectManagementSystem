8bfb1a85db04f191c93a97df536ff5b109f4f0ca
Sanket Panasuriya <sanket.panasuriya@bacancy.com>
2022-03-18 13:53:58 +0000

    Home controller added

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -74,4 +74,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def sign_up_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :roles)
   end
+  
+  def account_update_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :roles)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations', 
+    sessions: 'users/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
 end
