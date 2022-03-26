2a656ded51b2a37668342c617af8b4f0bdd95401
Sanket Panasuriya <sanket.panasuriya@bacancy.com>
2022-03-26 19:23:09 +0000

    Task Authorization and For project Client Autcustomer ion done

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,5 +1,11 @@
 class TaskController < ApplicationController
+    before_action :set_project
+    before_action :set_task, only: %i[ edit show]
     @@status = [['Created'],['On Going'], ['Submitted'], ['Re-Submitted'], ['Rejected'], ['Done']]
+    before_action :checking_authenticity, only: %i[update destroy create show index edit]
+    before_action :checking_authenticity_show, only: %i[ show ]
+    before_action :checking_authenticity_edit, only: %i[ edit ]
+    before_action :checking_authenticity_new, only: %i[ new ]
     def index
         @tasks = Task.joins(:sprint).where(sprint: {project_id: params[:project_id]})
     end
@@ -42,7 +48,6 @@ class TaskController < ApplicationController
     end
 
     def update
-        # params[:task][:status] = "assigned"
         @task = Task.find(params[:id])
         params[:sprint_id]=@task.sprint.id
         params[:employee_id] = @task.user_id
@@ -72,17 +77,48 @@ class TaskController < ApplicationController
     end
 
     private 
+        def set_project
+            @project = Project.find(params[:project_id])
+        end
 
+        def set_task
+            @task = Task.find(params[:id])
+        end
         def task_params
             params.require(:task).permit(:title, :sprint_id, :user_id, :due_date, :status, :description)
         end
 
         def getEmployee(employees = [])
-            User.joins(:roles).where(roles: {name: "employee"}).select(:id, :name).each {|employee| employees << [employee.name, employee.id]}
+            User.joins(:roles).where(roles: {name: "employee"}, delete_user: false).select(:id, :name).each {|employee| employees << [employee.name, employee.id]}
             employees
         end
         def getSprint(sprints = [])
             Sprint.all.where(project_id: params[:project_id]).each {|sprint| sprints << [sprint.title, sprint.id]}
             sprints
         end
+
+        def checking_authenticity
+            if (current_user.has_role? 'employee') || (current_user.has_role? 'customer')
+                render :file => 'public/403.html' unless can? :show, @project
+            else
+                render :file => 'public/403.html' unless can? :manage, Task
+            end
+        end
+
+        def checking_authenticity_show
+            render :file => 'public/403.html' unless can? :show, @task
+        end
+
+        def checking_authenticity_edit
+            if current_user.has_role? "manager"
+                render :file => 'public/403.html' unless can? :manage, Task
+            else
+                render :file => 'public/403.html' unless can? :edit, @task.user
+            end
+        end
+
+        def checking_authenticity_new
+            render :file => 'public/403.html' unless can? :new, Task
+        end
+
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -13,6 +13,8 @@ class Ability
     #     can :read, :all
     #   end
     #
+    arr = Task.select(:sprint_id).where(user_id: user.id).map {|x| x.sprint_id}.uniq
+    project_ids = Project.joins(:sprints).where(sprints: {id: arr}).ids
     can [:update, :edit], User, id: user.id
 
     if user.has_role? "admin"
@@ -20,16 +22,19 @@ class Ability
       can :manage, Role
       can :manage, Project
     elsif user.has_role? "manager"
-      # can :create, Project
       can :manage, Project, creator_id: user.id
-      
+      can :manage, Task
       
     elsif user.has_role? "employee"
-      
+      can [:show, :edit], Task, user_id: user.id
+      can [:show,:edit], Project do |project|
+        project_ids.include? project.id
+      end
+      can :update_task_status, Task, user_id: user.id
     else
       can :show, Project, client_id: user.id
       can :review_rating, Project
-
+      can :show, Task 
 
     end
 
