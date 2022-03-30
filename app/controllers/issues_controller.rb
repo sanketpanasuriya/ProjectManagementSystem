afe8f71b5ad9d1db64cc82e0212f3445f1b629e3
Yash Garala <yash.garala@bacancy.com>
2022-03-30 10:44:19 +0530

    issue change

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,17 +1,21 @@
 class IssuesController < ApplicationController
   before_action :set_issue, only: %i[ show edit update destroy ]
-
+  before_action :checking_authenticity_assign,only: %i[assign_issue_show assign_issue] 
+  
+  
   @@status = [['Created'],['On Going'], ['Submitted'], ['Re-Submitted'], ['Rejected'], ['Done']]
   @@type = [['Bug'],['Error']]
 
   # GET /issues or /issues.json
   def index
-    @issues = Issue.all
+    @project_id=params[:format]
+    
+    @issues = Issue.where(project_id: @project_id).all
   end
 
   # GET /issues/1 or /issues/1.json
   def show
-    # @issue=Issue.where(project_id: project_id).all
+    checking_authenticity(@issue,:show)
     @status = @@status
     @type = @@type
   end
@@ -33,21 +37,29 @@ class IssuesController < ApplicationController
 
   # GET /issues/1/edit
   def edit
+
+    checking_authenticity(@issue,:edit)
     @employee = []
     User.joins(:roles).where(roles: {name: "employee"}).select(:id, :name).each {|v| @employee << [v.name, v.id]}
     @status = @@status
     @type = @@type
+    p @issue
   end
 
   # POST /issues or /issues.json
   def create
+    
+    checking_authenticity(Issue.new,:create)
    
     @employee = []
     User.joins(:roles).where(roles: {name: "employee"}).select(:id, :name).each {|v| @employee << [v.name, v.id]}
    
     @status = @@status
     @type = @@type
-    @issue = Issue.new(issue_params)
+    temp=issue_params
+    temp["status"]="Created"
+  
+    @issue = Issue.new(temp)
     @issue.creator_id = current_user.id
 
     respond_to do |format|
@@ -63,6 +75,8 @@ class IssuesController < ApplicationController
 
   # PATCH/PUT /issues/1 or /issues/1.json
   def update
+
+    checking_authenticity(@issue,:update)
     @status = @@status
     @type = @@type
     respond_to do |format|
@@ -78,6 +92,8 @@ class IssuesController < ApplicationController
 
   # DELETE /issues/1 or /issues/1.json
   def destroy
+
+    checking_authenticity(@issue,:destroy)
     @issue.destroy
 
     respond_to do |format|
@@ -85,8 +101,58 @@ class IssuesController < ApplicationController
       format.json { head :no_content }
     end
   end
+  def assign_issue_show
+    issue_id=params[:format]
+    @issue=Issue.find(issue_id)
+    @employee = []
+    params[:status_id]=@issue.status
+    @status = @@status
+    User.joins(:roles).where(roles: {name: "employee"}).select(:id, :name).each {|v| @employee << [v.name, v.id]}
+    if(@issue.employee_id!=nil)
+      params[:selected_value]=@issue.employee_id
+    end
 
+  end
+  def assign_issue
+    @issue=Issue.find(params[:id])
+    @issue["status"]=params["issue"]["status"]
+    @issue["employee_id"]=params["issue"]["employee_id"]!=nil ?params["issue"]["employee_id"]:@issue["employee_id"]
+
+    respond_to do |format|
+      if @issue.save
+        format.html { redirect_to issue_url(@issue), notice: "" }
+        format.json { render :show, status: :created, location: @issue }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @issue.errors, status: :unprocessable_entity }
+      end
+    end
+    
+  end
+  def my_issue
+   
+  project_id = params[:format]
+  current_user_id=current_user.id
+
+  @issues=Issue.where(employee_id: current_user_id , project_id: project_id ).all
+  end
   private
+    def checking_authenticity(issue,opration)
+
+        render :file => 'public/403.html' unless can? opration, issue
+
+    end 
+    def checking_authenticity_assign
+      @issue = Issue.find(params[:format])
+      @project=Project.find(@issue.project_id)
+      if(current_user.roles.first.name=="manager")
+        
+        render :file => 'public/403.html' unless can? :assign_issue, @project
+      else 
+        render :file => 'public/403.html' unless can? :assign_issue, @issue
+        
+      end
+    end
     # Use callbacks to share common setup or constraints between actions.
     def set_issue
       @issue = Issue.find(params[:id])
@@ -94,6 +160,6 @@ class IssuesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def issue_params
-      params.require(:issue).permit(:project_id, :title, :description, :issue_type, :status, :employee_id)
+      params.require(:issue).permit(:project_id, :title, :description, :issue_type, :status)
     end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,19 +4,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
+   
     arr = Task.select(:sprint_id).where(user_id: user.id).map {|x| x.sprint_id}.uniq
     project_ids = Project.joins(:sprints).where(sprints: {id: arr}).ids
+     
     can [:update, :edit], User, id: user.id
+    can [:create,:show],Issue
 
+    can [:edit,:update,:destroy], Issue,creator_id: user.id
     if user.has_role? "admin"
       can [:create_user,:destroy, :new], User #Admin Can not update profile for other user  
       can :manage, Role
@@ -24,7 +19,7 @@ class Ability
       can :manage, Task
       can :manage, Sprint
       can :change_user_role, User
-
+      can :manage, Issue
     elsif user.has_role? "manager"
       # can :create, Project
       can %i[edit update destroy show project_status ], Project, creator_id: user.id
@@ -34,18 +29,22 @@ class Ability
       can :manage, Sprint, project: { creator_id: user.id}
 
       can :project_status, Project, creator_id: user.id
+      can :assign_issue, Project, creator_id: user.id # manager assign issue to other employee 
+
       
+
     elsif user.has_role? "employee"
       can [:show, :edit], Task, user_id: user.id
       can [:show,:edit], Project do |project|
         project_ids.include? project.id
       end
       can :update_task_status, Task, user_id: user.id
+      can [:assign_issue,:assign_issue_show], Issue,employee_id: user.id # employee can update thre own issue status 
+      
     else
       can :show, Project, client_id: user.id
       can :review_rating, Project
       can :show, Task 
-
     end
 
     # if user.manager?
